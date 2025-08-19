a13dead1fba62e23071751301189aa2c83a1311b
inakicarcereny <inakicarce@hotmail.com.ar>
2025-08-19 20:50:46 -0300

    Refactor layout styles and replace Geist fonts with Poppins for improved typography in RootLayout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { LoginButton } from '@/app/components/LoginButton';
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <main className="h-screen flex items-center justify-center bg-zinc-800">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl text-white font-semibold">
+          Welcome to NextAuth!
+        </h1>
+        <p className="text-zinc-200">
+          A simple authentication example for testing and demonstration
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
