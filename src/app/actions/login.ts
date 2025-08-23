466f9fb7e8c818721ec1d69a5047f372dee029f8
inakicarcereny <inakicarce@hotmail.com.ar>
2025-08-23 19:40:28 -0300

    Enhance login action to handle sign-in errors and redirect after successful login, improving user feedback on authentication issues

--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { LoginSchemaType, LoginSchema } from '@/app/schemas';
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: LoginSchemaType) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -9,5 +12,22 @@ export const login = async (values: LoginSchemaType) => {
     return { error: 'Invalid fields' };
   }
 
-  return { success: 'Email sent' };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid email or password' };
+        default:
+          return { error: 'Something went wrong' };
+      }
+    }
+  }
 };
