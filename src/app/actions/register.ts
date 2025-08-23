3d8e4488856e229763b5cb0f08436526e7c815d0
inakicarcereny <inakicarce@hotmail.com.ar>
2025-08-23 19:40:18 -0300

    Implement user registration logic, including email existence check and password hashing using bcrypt before creating a new user

--- src/app/actions/register.ts
+++ src/app/actions/register.ts
@@ -2,6 +2,10 @@
 
 import { RegisterSchemaType, RegisterSchema } from '@/app/schemas';
 
+import { db } from '@/lib/db';
+import bcrypt from 'bcrypt';
+import { getUserByEmail } from '@/app/data/user';
+
 export const register = async (values: RegisterSchemaType) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
@@ -9,5 +13,22 @@ export const register = async (values: RegisterSchemaType) => {
     return { error: 'Invalid fields' };
   }
 
-  return { success: 'Email sent' };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already exists!' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: 'User created' };
 };
