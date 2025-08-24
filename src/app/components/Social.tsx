7aa08225b641531f11410cae35cdc248f16fe681
inakicarcereny <inakicarce@hotmail.com.ar>
2025-08-23 21:41:46 -0300

    Implement social login with GitHub and Google in the Social component and configure providers in auth settings

--- src/app/components/Social.tsx
+++ src/app/components/Social.tsx
@@ -3,15 +3,23 @@
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
+import { signIn } from 'next-auth/react';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export function Social() {
+  const onClick = (provider: 'github' | 'google') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center justify-center w-full gap-2">
       <Button
         size="lg"
         className="w-[220px]"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick('google')}
       >
         <FcGoogle />
       </Button>
@@ -20,7 +28,7 @@ export function Social() {
         size="lg"
         className="w-[220px]"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClick('github')}
       >
         <FaGithub />
       </Button>
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,28 +1,33 @@
 import type { NextAuthConfig } from 'next-auth';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 import { LoginSchema } from '@/app/schemas';
 import { getUserByEmail } from '@/app/data/user';
 
 import Credentials from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
 
 export default {
   providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validateFields = LoginSchema.safeParse(credentials);
-
         if (validateFields.success) {
           const { email, password } = validateFields.data;
-
           const user = await getUserByEmail(email);
           if (!user || !user.password) return null;
-
           const passwordMatch = await bcrypt.compare(password, user.password);
-
           if (passwordMatch) return user;
         }
-
         return null;
       },
     }),
